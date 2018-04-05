22536e27683e561756db90ab166350708043a70d
Samuel Vangara <samuel.vangara@gmail.com>
2018-04-05 17:09:13 -0400

    rest service

--- restServiceQuestionaire/src/main/java/com/optum/caseManagement/controller/restController.java
+++ restServiceQuestionaire/src/main/java/com/optum/caseManagement/controller/restController.java
@@ -1,37 +1,45 @@
 package com.optum.caseManagement.controller;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.http.MediaType;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/questionaire")
 public class restController {
 	
-	/**
-	 * value = "/textArea",
-	 * @PathVariable String id
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/textArea",method = RequestMethod.GET)
-	public String getQuestionaire(Model model) {
-		
-		return "landingPage";
-}
-	/**
-	 * value = "/textArea",
-	 * @PathVariable String id
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/textArea",method = RequestMethod.POST)
-	public String postQuestionaire(Model model) {
-		
-		return "questionairePage";
-}
+    String dropDownType; 
+    ModelAndView modelView;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView LandingControllerPage(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException {
+		ModelAndView modelView = new ModelAndView("landingPage");
+		return modelView;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView LandingControllerPagepost(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException {
+		dropDownType = request.getParameter("type");
+		if(dropDownType.equals("textarea")) {
+	    modelView = new ModelAndView("TextArea");
+		}
+		else if(dropDownType.equals("radio")) {
+		    modelView = new ModelAndView("Radio");
+			}
+		else if(dropDownType.equals("checkbox")) {
+		    modelView = new ModelAndView("CheckBox");
+			}
+		else{
+		    modelView = new ModelAndView("landingPage");
+			}
+		return modelView;
+	}
+
 }
--- restServiceQuestionaire/src/main/webapp/WEB-INF/uiPages/landingPage.jsp
+++ restServiceQuestionaire/src/main/webapp/WEB-INF/uiPages/landingPage.jsp
@@ -1,36 +1,46 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Questionaire</title>
+<title>Questionnaire</title>
 </head>
 <body>
-	<form name="questionaire" action="questionaire" method="post/{value}">
-
+	<h3>Questionnaire Search Criteria</h3>
+	<form name="search" action="search" method="post">
 		<table align="center">
-		<tr>
-		<td colspan="2">
-		<c:out value="${error}"/>
-		</td>
-		</tr>
-		<tr>
-		<td>
-		<select id="type" name="type" >
-	     <option value="none">---Select---</option>
-		<option value="textarea">TextArea</option>
-		<option value="radio">Radio</option>
-		<option value="checkbox">CheckBox</option>
-		</select>
-		</td>
-		</tr>
+			<tr>
+				<td>Questionnaire Name:</td>
+				<td>Task Type:</td>
+
+			</tr>
+			<tr>
+				<td><input type="text" name="questionnairename" /></td>
+				<td><select id="type" name="type">
+						<option name="none" id="none" value="none">---Select---</option>
+						<option name="textarea" id="textarea" value="textarea">TextArea</option>
+						<option name="radio" id="radio" value="radio">Radio</option>
+						<option name="checkbox" id="checkbox" value="checkbox">CheckBox</option>
+				</select></td>
+			</tr>
 			<tr>
 				<td align="center" colspan="2">
-					<button type="Submit">Get Questionaire</button>
+					<button type="Submit">Search</button>
+				</td>
+			</tr>
+		</table>
+		</form>
+		<form name="questionaire" action="questionaire" method="post">
+		<table>
+		<tr>
+				<td align="center" colspan="2">
+					<button type="Submit">Add Questionnaire</button>
 				</td>
 			</tr>
 		</table>
 	</form>
+
+
 </body>
 </html>
